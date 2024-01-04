b5753713c05976092e84c64a570dad510f050ab2
Ryan Gurnick <ryangurnick@gmail.com>
2024-01-03 22:53:40 -0800

    Adding cities model and migration

---
+++ app/Models/Reference/City.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models\Reference;
+
+use Illuminate\Database\Eloquent\Model;
+
+class City extends Model
+{
+    protected $fillable = [
+        'name'
+    ];
+}
---
+++ database/migrations/2024_01_03_040536_create_cities_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Models\Reference\Country;
+use App\Models\Reference\State;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cities', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Country::class)
+                ->constrained();
+            $table->foreignIdFor(State::class)
+                ->constrained();
+            $table->string('name');
+            $table->string('latitude');
+            $table->string('longitude');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cities');
+    }
+};
