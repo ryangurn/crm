a969b20b8a9ec5f30c74f336370ff17e734df08a
Ryan Gurnick <ryangurnick@gmail.com>
2024-01-04 23:37:23 -0800

    Adding horizon specific configuration

--- config/horizon.php
+++ config/horizon.php
@@ -182,7 +182,7 @@ return [
     'defaults' => [
         'supervisor-1' => [
             'connection' => 'redis',
-            'queue' => ['default'],
+            'queue' => ['urgent', 'high', 'medium', 'low'],
             'balance' => 'auto',
             'autoScalingStrategy' => 'time',
             'maxProcesses' => 1,
